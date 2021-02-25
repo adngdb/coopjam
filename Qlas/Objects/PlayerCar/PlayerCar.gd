3be8c4bafe6ffa40915ceaa7303844c272d53661
Adrian Gaudebert <adrian@gaudebert.fr>
2021-02-25 19:57:38 +0100

    Add a better Road tile. Make sure camera is always centered on the road, not following the player on the x axis.

--- Qlas/Objects/PlayerCar/PlayerCar.gd
+++ Qlas/Objects/PlayerCar/PlayerCar.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export var max_speed := 1000.0
 export var inc_speed := 150.0
 export var dec_speed := 300.0
+export var in_lane_pos := 65.0
 
 var speed := 0.0
 var motion = Vector2(0.0, -1.0)
@@ -25,10 +26,12 @@ func _physics_process(delta):
 	
 	move_and_slide(speed * motion)
 	pos_check()
+	# Make sure camera is always centered (and does not follow the car on the x axis)
+	$Camera2D.offset.x = -global_position.x
 
 
 func pos_check():
-	global_position.x = clamp(global_position.x, -130.0, 130.0)
+	global_position.x = clamp(global_position.x, -in_lane_pos, in_lane_pos)
 	
 
 func move_lane_right():
