d08ca4d44b6c6131aad8b51e129f5b128075d38e
Adrian Gaudebert <adrian@gaudebert.fr>
2021-02-25 11:19:02 +0100

    Implement basic mechanics for a first prototype: touch to increase speed, speed decreases over time.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
-console.log('coucou');
+// Constants.
+const MAX_SPEED = 100;
+const SPEED_INCREMENT = 10;
+const SPEED_DECREASE_PER_SECOND = 20;
+
+// Variables we need.
+let speed = 0;
+let previousTimestamp = null;
+
+
+// On touch, increase speed.
+window.addEventListener('touchend', () => {
+    speed += SPEED_INCREMENT;
+    if (speed > MAX_SPEED) {
+        speed = MAX_SPEED;
+    }
+}, false);
+
+
+function decreaseSpeed(elapsedTime) {
+    speed = speed - (elapsedTime / 1000 * SPEED_DECREASE_PER_SECOND);
+    if (speed < 0) {
+        speed = 0;
+    }
+}
+
+function step(timestamp) {
+    if (previousTimestamp === null) {
+        previousTimestamp = timestamp;
+    }
+    let elapsed = timestamp - previousTimestamp;
+    previousTimestamp = timestamp;
+
+    decreaseSpeed(elapsed);
+
+    console.log(speed);
+    window.requestAnimationFrame(step);
+}
+
+window.requestAnimationFrame(step);
